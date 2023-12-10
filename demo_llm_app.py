403b6f983a1158ed989d6b828a28d676118047be
Vo Hieu <vominhhieu24022002@gmail.com>
2023-12-10 21:57:32 +0700

    remove the package pyxlsb and xlsxwriter

--- demo_llm_app.py
+++ demo_llm_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from io import BytesIO
-from pyxlsb import open_workbook as open_xlsb
-import xlsxwriter
+# from pyxlsb import open_workbook as open_xlsb
+# import xlsxwriter
 from langchain.chat_models import ChatOpenAI
 from kor.extraction import create_extraction_chain
 from kor.nodes import Object, Text, Number
